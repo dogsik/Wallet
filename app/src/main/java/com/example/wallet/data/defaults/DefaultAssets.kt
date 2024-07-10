4de7498861b1067afb86d862d7e2d2709034cb90
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-07-10 19:43:23 +0300

    asset models

---
+++ app/src/main/java/com/example/wallet/data/defaults/DefaultAssets.kt
@@ -0,0 +1,73 @@
+package com.example.wallet.data.defaults
+
+import com.example.wallet.domain.entity.Bond
+import com.example.wallet.domain.entity.Cash
+import com.example.wallet.domain.entity.Currency
+import com.example.wallet.domain.entity.Meta
+import com.example.wallet.domain.entity.Stock
+import java.math.BigDecimal
+import java.time.LocalDate
+
+object AssetData {
+    fun getAssets() = listOf(
+        Stock(
+            id = 1,
+            name = "PKNORLEN",
+            purchasePrice = BigDecimal(65.80),
+            currency = Currency.USD,
+            meta = Meta("Poland"),
+            dividend = BigDecimal(4.15),
+            paymentDate = LocalDate.of(2024, 12, 20)
+        ),
+        Stock(
+            id = 2,
+            name = "NVIDIA Corporation",
+            purchasePrice = BigDecimal(134.57),
+            currency = Currency.USD,
+            meta = Meta("USA"),
+            dividend = BigDecimal(0.01),
+            paymentDate = LocalDate.of(2024, 11, 6)
+        ),
+        Bond(
+            id = 3,
+            name = "RGBI",
+            purchasePrice = BigDecimal(105.16),
+            currency = Currency.RUB,
+            meta = Meta("Russia"),
+            profitability = BigDecimal(15.92),
+            maturityDate = LocalDate.of(2024, 9, 1)
+        ),
+        Cash(
+            id = 4,
+            name = "1 EUR",
+            purchasePrice = BigDecimal(3.4692),
+            currency = Currency.BYN,
+            meta = Meta("Belarus"),
+            salePrice = BigDecimal(3.4654)
+        ),
+        Cash(
+            id = 5,
+            name = "1 USD",
+            purchasePrice = BigDecimal(3.21),
+            currency = Currency.BYN,
+            meta = Meta("Belarus"),
+            salePrice = BigDecimal(3.202)
+        ),
+        Cash(
+            id = 6,
+            name = "100 RUB",
+            purchasePrice = BigDecimal(3.6385),
+            currency = Currency.BYN,
+            meta = Meta("Belarus"),
+            salePrice = BigDecimal(3.615)
+        ),
+        Cash(
+            id = 7,
+            name = "1 CNY",
+            purchasePrice = BigDecimal(7.269),
+            currency = Currency.BYN,
+            meta = Meta("Belarus"),
+            salePrice = BigDecimal(7.3007)
+        )
+    )
+}
---
+++ app/src/main/java/com/example/wallet/domain/entity/Assets.kt
@@ -0,0 +1,53 @@
+package com.example.wallet.domain.entity
+
+import java.math.BigDecimal
+import java.time.LocalDate
+
+enum class Currency(val currencyName: String) {
+    USD("usd"),
+    EUR("eur"),
+    CNY("cny"),
+    RUB("rub"),
+    BYN("byn")
+}
+
+data class Meta(
+    val country: String
+)
+
+abstract class Asset(
+    open val id: Int,
+    open val name: String,
+    open val purchasePrice: BigDecimal,
+    open val currency: Currency,
+    open val meta: Meta
+)
+
+data class Stock(
+    override val id: Int,
+    override val name: String,
+    override val purchasePrice: BigDecimal,
+    override val currency: Currency,
+    override val meta: Meta,
+    val dividend: BigDecimal,
+    val paymentDate: LocalDate
+) : Asset(id, name, purchasePrice, currency, meta)
+
+data class Bond(
+    override val id: Int,
+    override val name: String,
+    override val purchasePrice: BigDecimal,
+    override val currency: Currency,
+    override val meta: Meta,
+    val profitability: BigDecimal,
+    val maturityDate: LocalDate
+) : Asset(id, name, purchasePrice, currency, meta)
+
+data class Cash(
+    override val id: Int,
+    override val name: String,
+    override val purchasePrice: BigDecimal,
+    override val currency: Currency,
+    override val meta: Meta,
+    val salePrice: BigDecimal
+) : Asset(id, name, purchasePrice, currency, meta)
