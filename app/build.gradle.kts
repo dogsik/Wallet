902757371e67385dadd03cb47d4733a28c0dd0c8
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-07-10 19:50:18 +0300

    resolve issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.androidx.navigation.safe.args)
+    alias(libs.plugins.hilt.android)
+    id("com.google.devtools.ksp")
 }
 
 apply(from = "../code_quality/detekt.gradle")
