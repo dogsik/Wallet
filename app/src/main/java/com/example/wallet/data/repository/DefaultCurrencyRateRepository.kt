6c3f7ce481dc85be567c84b8fba0ece2d89ca1ed
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-08-14 17:43:16 +0300

    add utils

--- app/src/main/java/com/example/wallet/data/repository/DefaultCurrencyRateRepository.kt
+++ app/src/main/java/com/example/wallet/data/repository/DefaultCurrencyRateRepository.kt
@@ -4,10 +4,10 @@ import com.example.wallet.data.local.LocalCurrencyRateDataSource
 import com.example.wallet.data.mapper.CurrencyRateMapper
 import com.example.wallet.data.model.CurrencyRateModel
 import com.example.wallet.domain.repository.CurrencyRateRepository
+import com.example.wallet.utils.resultOf
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
-import kotlin.coroutines.cancellation.CancellationException
 
 class DefaultCurrencyRateRepository @Inject constructor(
     private val localCurrencyRateDataSource: LocalCurrencyRateDataSource
@@ -19,16 +19,6 @@ class DefaultCurrencyRateRepository @Inject constructor(
                     localCurrencyRateDataSource.getCurrencyRate(code)
                 )
             }
-        }
-    }
-
-    private inline fun <T, R> T.resultOf(block: T.() -> R): Result<R> {
-        return try {
-            Result.success(block())
-        } catch (e: CancellationException) {
-            throw e
-        } catch (e: Exception) {
-            Result.failure(Exception("Fetching error"))
         }
     }
 }
